f9206b2b70e3db38ac3e0c5e659325e8172dec77
delwarde <damien.delwarde@gmail.com>
2012-10-16 21:32:20 +0200

    comments corrections

--- RGBCube.dart
+++ RGBCube.dart
@@ -1,20 +1,3 @@
-/**
- * RGB colors define a discrete cube of size 256. Choosing a RGB color is choosing
- *  a point in that cube.
- *  This example show you an original version of the RGB cube exploration :
- *  We travel along the main diagonal of the cube via the slider element, 
- *  and see the intersection of the cube with the plane that is perpendicular 
- *  to this diagonal at the considered point.
- *  If we click inside the intersection, we pick a color and see it.
- *  
- *  This is not a perfect representation of the cube, because of some approximations :
- *  If we choose a point on one edge, we have a plane that is perpendicular to the diagonal
- *  and contains this point. But we are not sure that this plane also contains a
- *  point on the diagonal (we are in a discrete space !).
- *  Anyway, I decide to represent the intersection between the cube and the plane,
- *  and fill it with appropriate colors (calculated as a barycenter), without considering
- *  if the point really exists. So this is just an approximative representation...
- */
 #import('dart:html');
 #import('dart:math');
 
@@ -78,11 +61,11 @@ class ColorCube {
    * Draw an equilateral triangle.
    * The 3 points [p1], [p2] and [p3] define the vertex of the complete triangle ;
    * [A], [B] and [C] are vertex's colors.
-   * The drawing has a horizontal basis ([p1.x], [p1.y]) --> ([p2.x], [p1.y]).
+   * The drawing has a horizontal basis ([p1[0]], [p1[1]]) --> ([p2[0]], [p1[1]]).
    * When [orientation] is true, the triangle is drawn above his basis,
    * else it's drawn below the basis.
-   * To draw a complete triangle set [ymax] = [p3.y].
-   * To truncate the triangle, set [ymax] < [p3.y] (if [orientation] is true).
+   * To draw a complete triangle set [ymax] = [p3[1]].
+   * To truncate the triangle, set [ymax] < [p3[1]] (if [orientation] is true).
    */
   void drawColorTriangle(ImageData imgData,
                          List p1, List p2, List p3,
